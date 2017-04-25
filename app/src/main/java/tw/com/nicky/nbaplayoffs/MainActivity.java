9cbbcd8f15d989b05577795a51cd1ba7da7bbdfc
fcu-d0441320 <d0441320@fcu.edu.tw>
2017-04-25 21:04:59 +0800

    收工

--- app/src/main/java/tw/com/nicky/nbaplayoffs/MainActivity.java
+++ app/src/main/java/tw/com/nicky/nbaplayoffs/MainActivity.java
@@ -1,5 +1,6 @@
 package tw.com.nicky.nbaplayoffs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +24,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ArrayList<NBAGame> gameList = new ArrayList<NBAGame>();
+        gameList.add(new NBAGame(R.drawable.boyroad,"公鹿",93,R.drawable.bowdragon,"暴龍",118));
+        gameList.add(new NBAGame(R.drawable.man,"勇士",128,R.drawable.tohungpeople,"拓荒者",103));
+        Adapter adapter =
+                new Adapter(this, gameList);
+        listView =(ListView)findViewById(R.id.list);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(itemclick);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +42,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+   AdapterView.OnItemClickListener itemclick = new AdapterView.OnItemClickListener() {
+       @Override
+       public void onItemClick(AdapterView<?> av, View v,
+                               int position, long id) {
+
+
+       }
+   };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/tw/com/nicky/nbaplayoffs/NBAGame.java
@@ -0,0 +1,19 @@
+package tw.com.nicky.nbaplayoffs;
+
+/**
+ * Created by user on 2017/4/25.
+ */
+
+public class NBAGame {
+    String T1Name,T2Name;
+    int T1Point,T2Point;
+    int T1Pic,T2Pic;
+    NBAGame(int T1Pic,String T1Name,int T1Point,int T2Pic,String T2Name,int T2Point){
+        this.T1Name=T1Name;
+        this.T1Pic=T1Pic;
+        this.T1Point=T1Point;
+        this.T2Name=T2Name;
+        this.T2Pic=T2Pic;
+        this.T2Point=T2Point;
+    }
+}
